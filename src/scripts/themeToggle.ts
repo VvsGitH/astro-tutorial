0ab0af6ac123286a3e1f43476d5d4154e9ac362f
Vito Paparella Santorsola <vito.paparella@rcsinnovation.it>
2024-10-18 14:23:56 +0200

    Feat: added ViewTransitions

--- src/scripts/themeToggle.ts
+++ src/scripts/themeToggle.ts
@@ -1,27 +1,47 @@
-const theme = (() => {
+function getTheme() {
   if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
     return localStorage.getItem("theme");
   }
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    window.localStorage.setItem("theme", "dark");
     return "dark";
   }
+  window.localStorage.setItem("theme", "light");
   return "light";
-})();
-
-if (theme === "light") {
-  document.documentElement.classList.remove("dark");
-} else {
-  document.documentElement.classList.add("dark");
 }
 
-window.localStorage.setItem("theme", theme!);
+function applyTheme() {
+  const theme = getTheme();
+  if (theme === "light") {
+    document.documentElement.classList.remove("dark");
+  } else {
+    document.documentElement.classList.add("dark");
+  }
+}
 
-const handleToggleClick = () => {
-  const element = document.documentElement;
-  element.classList.toggle("dark");
+function initThemeToggle() {
+  const handleToggleClick = () => {
+    const element = document.documentElement;
+    element.classList.toggle("dark");
 
-  const isDark = element.classList.contains("dark");
-  localStorage.setItem("theme", isDark ? "dark" : "light");
-};
+    const isDark = element.classList.contains("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+  };
 
-document.getElementById("themeToggle")?.addEventListener("click", handleToggleClick);
+  document.getElementById("themeToggle")?.addEventListener("click", handleToggleClick);
+}
+
+/**
+ * Apply theme and set events on page load
+ */
+document.addEventListener("astro:page-load", () => {
+  applyTheme();
+  initThemeToggle();
+});
+
+/**
+ * Re-apply theme on page-transition
+ */
+document.addEventListener("astro:after-swap", () => {
+  applyTheme();
+});
